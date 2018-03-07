cdbfb71eb7e7290f614e614cd17f5d4d9c22912b
Ming Chen <mingchen62@gmail.com>
2018-03-07 08:36:22 -0500

    change to cpu

--- app.sh
+++ app.sh
@@ -1,1 +1,2 @@
-~/miniconda2/bin/python app.py -model im2text-hw-model_acc_96.75_ppl_1.16_e13.pt  -data_type img -replace_unk -verbose -n_best 3 -batch_size 1 -gpu 0 
+#~/miniconda2/bin/python app.py -model im2text-hw-model_acc_96.75_ppl_1.16_e13.pt  -data_type img -replace_unk -verbose -n_best 3 -batch_size 1 -gpu 0 
+python app.py -model im2text-hw-model_acc_96.75_ppl_1.16_e13.pt  -data_type img -replace_unk -verbose -n_best 3 -batch_size 1 
